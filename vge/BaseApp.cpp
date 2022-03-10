e94826df6aac556a9cbbdaa9b338be9ed1a60d56
VanderleiJordaoJr <vanderlei.jordao.jr@gmail.com>
2022-03-09 23:26:08 -0300

    Push Constants

--- vge/BaseApp.cpp
+++ vge/BaseApp.cpp
@@ -1,11 +1,19 @@
 #include "BaseApp.h"
 
-// std
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
 #include <array>
 #include <stdexcept>
 
 namespace vge
 {
+	struct PushConstantData
+	{
+		glm::vec2 offset;
+		alignas(16) glm::vec3 color;
+	};
 
 	BaseApp::BaseApp()
 	{
@@ -66,12 +74,17 @@ namespace vge
 
 	void BaseApp::CreatePipelineLayout()
 	{
+		VkPushConstantRange pushConstantRange{};
+		pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+		pushConstantRange.offset = 0;
+		pushConstantRange.size = sizeof(PushConstantData);
+
 		VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
 		pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
 		pipelineLayoutInfo.setLayoutCount = 0;
 		pipelineLayoutInfo.pSetLayouts = nullptr;
-		pipelineLayoutInfo.pushConstantRangeCount = 0;
-		pipelineLayoutInfo.pPushConstantRanges = nullptr;
+		pipelineLayoutInfo.pushConstantRangeCount = 1;
+		pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
 		if (vkCreatePipelineLayout(vgeDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) !=
 			VK_SUCCESS)
 		{
@@ -133,6 +146,8 @@ namespace vge
 
 	void BaseApp::RecordCommandBuffer(int imageIndex)
 	{
+		static int frame = 0;
+		frame = (frame + 1) % 1000;
 		VkCommandBufferBeginInfo beginInfo{};
 		beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 
@@ -150,7 +165,7 @@ namespace vge
 		renderPassInfo.renderArea.extent = vgeSwapChain->getSwapChainExtent();
 
 		std::array<VkClearValue, 2> clearValues{};
-		clearValues[0].color = {0.1f, 0.1f, 0.1f, 1.0f};
+		clearValues[0].color = {0.01f, 0.01f, 0.01f, 1.0f};
 		clearValues[1].depthStencil = {1.0f, 0};
 		renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
 		renderPassInfo.pClearValues = clearValues.data();
@@ -170,7 +185,23 @@ namespace vge
 
 		vgePipeline->Bind(commandBuffers[imageIndex]);
 		vgeModel->Bind(commandBuffers[imageIndex]);
-		vgeModel->Draw(commandBuffers[imageIndex]);
+
+		for (int j = 0; j < 4; j++)
+		{
+			PushConstantData push{};
+			push.offset = {-0.5f + frame * 0.009f, -0.4f + j * 0.25f};
+			push.color = {0.0f, 0.0f, 0.2f + 0.2f * j};
+
+			vkCmdPushConstants(
+				commandBuffers[imageIndex],
+				pipelineLayout,
+				VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
+				0,
+				sizeof(PushConstantData),
+				&push);
+
+			vgeModel->Draw(commandBuffers[imageIndex]);
+		}
 
 		vkCmdEndRenderPass(commandBuffers[imageIndex]);
 		if (vkEndCommandBuffer(commandBuffers[imageIndex]) != VK_SUCCESS)
